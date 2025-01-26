07af2f17529c3297d8905d2720cf1122d393b9d9
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-01-26 17:28:53 +0300

    context ve migration oluşturuldu

--- MyPortfolio/DAL/Entities/Experience.cs
+++ MyPortfolio/DAL/Entities/Experience.cs
@@ -1,8 +1,8 @@
 ﻿namespace MyPortfolio.DAL.Entities
 {
-    public class Experince
+    public class Experience
     {
-        public int ExperinceId { get; set; }
+        public int ExperienceId { get; set; }
         public string Head { get; set; }
         public string Title { get; set; }
         public string Date { get; set; }
