9ab62e17fb7db7a636433e6bf5e1a6808bed20bd
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-01-27 22:28:50 +0300

    Feature ve About Verilerinin DB'den Çekilmesi

--- MyPortfolio/ViewComponents/_AboutComponentPartial.cs
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _AboutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly MyPortfolioContext _context;
+
+        public _AboutComponentPartial(MyPortfolioContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle=_context.Abouts.Select(x => x.Title).ToList().FirstOrDefault();
+            ViewBag.aboutDescription=_context.Abouts.Select(x=>x.SubDescription).ToList().FirstOrDefault();
+            ViewBag.aboutDetail=_context.Abouts.Select(x=>x.Details).ToList().FirstOrDefault();
+            return View();
+        }
     }
 }
--- MyPortfolio/ViewComponents/_FeatureComponentPartial.cs
+++ MyPortfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
-    public class _FeatureComponentPartial:ViewComponent
+    public class _FeatureComponentPartial : ViewComponent
     {
+        private readonly MyPortfolioContext _context;
+
+        public _FeatureComponentPartial(MyPortfolioContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _context.Features.ToList();
+            return View(values);
         }
     }
 }
