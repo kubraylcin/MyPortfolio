51c47c2a37a75b6781e66a1881492aef63455bc9
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-01-31 23:39:07 +0300

    Yeni Deneyim Ekleme işlemi yapıldı

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+using System.Linq;
 
 namespace MyPortfolio.Controllers
 {
@@ -12,10 +14,31 @@ namespace MyPortfolio.Controllers
 			_context = context;
 		}
 
+		// Tüm deneyimleri listeleyen sayfa
 		public IActionResult ExperienceList()
 		{
 			var experiences = _context.Experiences.ToList();
 			return View(experiences);
+		}
+
+		// Yeni deneyim ekleme formunu açar
+		[HttpGet]
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+
+		// Yeni deneyimi veritabanına ekler butona tıklandığında çalışır
+		[HttpPost]
+		public IActionResult CreateExperience(Experience experience)
+		{
+			if (ModelState.IsValid) // Formun doğruluğunu kontrol et
+			{
+				_context.Experiences.Add(experience); // Burada `context` yerine `_context` kullandım
+				_context.SaveChanges();
+				return RedirectToAction("ExperienceList");
+			}
+			return View(experience); // Hata varsa aynı sayfaya geri dön
 		}
 	}
 }
