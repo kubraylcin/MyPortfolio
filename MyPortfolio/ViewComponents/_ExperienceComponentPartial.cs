8587136501fe7122c3800f64ff749551b8bb5f9c
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-01-31 15:54:58 +0300

    Yeteneklerin Listelenmesi

--- MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
-    public class _ExperienceComponentPartial:ViewComponent
+    public class _ExperienceComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly MyPortfolioContext _context;
+
+        public _ExperienceComponentPartial(MyPortfolioContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var experiences = _context.Experiences.ToList();
+            return View(experiences);
+        }
     }
 }
