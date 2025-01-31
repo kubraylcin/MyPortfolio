2b1b7c7b31d6f27728cd699070b7001ef7c47af5
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-01-31 23:00:56 +0300

    Deneyimler Sayfasına Giriş

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.Controllers
 {
-    public class ExperienceController : Controller
-    {
-        public IActionResult ExperienceList()
-        {
-            return View();
-        }
-    }
+	public class ExperienceController : Controller
+	{
+		private readonly MyPortfolioContext _context;
+
+		public ExperienceController(MyPortfolioContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult ExperienceList()
+		{
+			var experiences = _context.Experiences.ToList();
+			return View(experiences);
+		}
+	}
 }
