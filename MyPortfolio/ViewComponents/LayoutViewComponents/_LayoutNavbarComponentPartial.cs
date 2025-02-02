5d5a39d7b4bbce76165f7d300d7f2b755351803f
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-02-02 11:29:25 +0300

    Bildirimlerin Düzenlenmesi işlemi yapıldı

--- MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents.LayoutViewComponents
 {
 	public class _LayoutNavbarComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke() { return View(); }
+		private readonly MyPortfolioContext _context;
+
+		public _LayoutNavbarComponentPartial(MyPortfolioContext context)
+		{
+			_context = context;
+		}
+		public IViewComponentResult Invoke()
+		{
+			//yapılacak olanların toplam sayıısı
+			ViewBag.toDolistCount=_context.ToDoLists.Where(x=>x.Status==false).Count();
+			//yapılmamış bildirimleri getirmek için
+			var values = _context.ToDoLists.Where(x => x.Status == false).ToList();
+			return View(values);
+		}
 	}
 }
